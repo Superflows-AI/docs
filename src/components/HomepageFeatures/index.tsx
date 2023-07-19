8b75697b5bb29f3b43acdb2125715ee33faacc39
MGPhillips <matthew.phillips.12@ucl.ac.uk>
2023-07-19 14:55:03 +0100

    Small updates to homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,31 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Simple Setup',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Just upload an API Spec, connect your API, and get a functional 
+        Copilot for your product
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Low-code Control Dashboard',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Configure and evaluate your Copilot in a simple to use dashboard
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Plug and play UI Components',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Add Superflows to your product in a few lines of code with our 
+        customisable UI components
       </>
     ),
   },
